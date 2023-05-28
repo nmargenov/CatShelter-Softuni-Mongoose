fca919992f490d005fea90e884163a05b3943b9f
nmargenov <nikolay_margenov@yahoo.com>
2023-05-28 21:05:43 +0300

    Setup handlebars engine

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
+app.engine('hbs',handlebars.engine({
+    extname:'hbs',
+    runtimeOptions:{
+        allowProtoMethodsByDefault: true,
+        allowProtoPropertiesByDefault: true
+    }
+    //runtimeOptions will allow me to get the properties later from mongoose
+}));
+app.set('view engine','hbs');
+app.set('views','src/views');
+
+
 app.get(['/','/index'],(req,res)=>{
     res.write('Working properly!');
     res.end();
