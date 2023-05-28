c17d57808e0b3aa429626d9290c05e6711c0dcc5
nmargenov <nikolay_margenov@yahoo.com>
2023-05-28 22:39:13 +0300

    Handles invalid addCat requests

--- src/controllers/catsController.js
+++ src/controllers/catsController.js
@@ -25,7 +25,18 @@ router.get('/addCat',async (req,res)=>{
 });
 
 router.post('/addCat',async (req,res)=>{
+    const name = req.body.name.trim();
+    const description = req.body.description.trim();
+    const imageUrl = req.body.imageUrl.trim();
+    const breed = req.body.breed.trim();
     
+    if(name == "" || description =="" || imageUrl == "" || breed == ""){
+        const breeds = await getAllBreeds();
+        res.status(400);
+        res.render('addCat',{breeds,name,description,imageUrl,breed});
+    }else{
+        res.end();
+    }
 });
 
 module.exports = router;
