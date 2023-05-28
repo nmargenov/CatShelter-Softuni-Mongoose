2d24441d08b3d34be35b84e6c204925d8c9985a2
nmargenov <nikolay_margenov@yahoo.com>
2023-05-28 20:59:41 +0300

    Set up express
    Set up express in server.js + / and /index routes

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-console.log('Working properly!');
+const express = require('express');
+
+const app = express();
+app.get(['/','/index'],(req,res)=>{
+    res.write('Working properly!');
+    res.end();
+});
+const PORT = 5000;
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}...`));
